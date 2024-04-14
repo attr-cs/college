9eac3516f70532310843d2186edd7f11c5df3ef9
attr-cs <attr-cs@users.noreply.github.com>
2024-04-14 21:24:54 +0530

    removing

--- js/script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("sellerForm").addEventListener("submit", function(event) {
-    var sellerName = document.getElementById("sellerName").value;
-    var phone = document.getElementById("phone").value;
-    var address = document.getElementById("address").value;
-    if (sellerName === '' || phone === '' || address === '') {
-        alert('Please fill in all fields');
-        event.preventDefault();
-    }
-});
